68f5fcbbed0e8760ba10762ce30c8df8d2693f9a
rmsalindra <ramanjulu.salindra@gmail.com>
2014-12-05 15:26:23 +0530

    conflistc

--- src/Sample.java
+++ src/Sample.java
@@ -5,10 +5,10 @@ public class Sample {
 		// TODO Auto-generated method stub
 		System.out.println("test for update");
 		System.out.println("Same line, multiple changes Again");
-		System.out.println("one more line for conflict");
 		System.out.println("second line for conflict");
 		System.out.println("third line for cl");
 		System.out.println("from my machine");
+		System.out.println("I removed one line for conflict.");
 	}
 
 }
