21198489bcd9a73800fa545a85ff2c32a652f5e1
carAmeen <ameen.5504@gmail.com>
2014-12-05 14:40:41 +0530

    cxvcx

--- src/TestSys.java
+++ src/TestSys.java
@@ -5,7 +5,6 @@ public class TestSys {
 		System.out.println("TEST PROJECT IN GITHUB");
 		System.out.println("EDITTED FROM GITHUB");
 		System.out.println("EDITTED FROM VEERU");
-		System.out.println("NAVIN");
 		System.out.println("Next Change");
 		System.out.println("RAMA");
 		System.out.println("AFTER INVITING RAMA");
