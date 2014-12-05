12fa573c7afb30f9e39f3e236275e9c4a14f1212
carAmeen <ameen.5504@gmail.com>
2014-12-05 15:34:48 +0530

    dsafasd

--- src/Sample.java
+++ src/Sample.java
@@ -5,10 +5,6 @@ public class Sample {
 		// TODO Auto-generated method stub
 		System.out.println("test for update");
 		System.out.println("Same line, multiple changes Again");
-		System.out.println("second line for conflict");
-		System.out.println("third line for cl");
-		System.out.println("from my machine");
-		System.out.println("I removed one line for conflict.");
 		System.out.println("RAMA");
 	}
 
