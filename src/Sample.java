e832e103932cdf063aaa6e1a2197ccf7c0bf9ef1
carAmeen <ameen.5504@gmail.com>
2014-12-05 15:17:48 +0530

    fdsaf

--- src/Sample.java
+++ src/Sample.java
@@ -4,7 +4,7 @@ public class Sample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("test for update");
-		System.out.println("Same line ");
+		System.out.println("Same line, multiple changes");
 		System.out.println("one more line for conflict");
 	}
 
