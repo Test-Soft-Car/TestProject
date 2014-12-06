2ac8b745970137e14ff20490212e338c7a47f279
carAmeen <ameen.5504@gmail.com>
2014-12-06 14:34:31 +0530

    Comment Veeru

--- src/BranchClass.java
+++ src/BranchClass.java
@@ -4,7 +4,7 @@ public class BranchClass {
 	public static void main(String[] args) {
 		System.out.println("Branch Class created");
 		System.out.println("Classes of Branch From Git");
-		
+		System.out.println("Veeru");
 	}
 
 }
