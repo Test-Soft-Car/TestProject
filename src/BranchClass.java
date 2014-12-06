94dce80608316b464f54c7bc758c4089b50283f0
carAmeen <carAmeen@users.noreply.github.com>
2014-12-06 11:28:44 +0530

    Update BranchClass.java
    From Github

--- src/BranchClass.java
+++ src/BranchClass.java
@@ -3,7 +3,7 @@ public class BranchClass {
 
 	public static void main(String[] args) {
 		System.out.println("Branch Class created");
-		System.out.println("Classes of Branch");
+		System.out.println("Classes of Branch From Git");
 		
 	}
 
